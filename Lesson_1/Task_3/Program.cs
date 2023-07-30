a192fabae55b3d66e2cca5022745bdcebfd19731
Kewaaaa <inokenty119228@gmail.com>
2023-07-31 00:16:21 +0300

    Dublicate items in list

---
+++ Lesson_1/Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] nums = new int[10];
+
+for (int i = 0;i < nums.Length; i++)
+{
+    Console.Write($"Enter {i+1} number: ");
+    int buffer = int.Parse(Console.ReadLine());
+    nums[i] = buffer;
+    
+}
+Console.Write("Your list - ");
+for (int j = 0; j < nums.Length; j++)
+{
+    Console.Write(nums[j]+" ");
+}
+Console.Write("\nList that is requared - ");
+for (int k = 0; k < nums.Length; k++)
+{
+    Console.Write(nums[k]+ " " + nums[k]+" ");
+}
