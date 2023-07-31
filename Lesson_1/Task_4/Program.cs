2e48556476841e421186cb049765fb76e61475ec
Kewaaaa <inokenty119228@gmail.com>
2023-07-31 23:19:09 +0300

    String combination

---
+++ Lesson_1/Task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Input first string: ");
+string string1 = Console.ReadLine();
+Console.Write("Input second string: ");
+string string2 = Console.ReadLine();
+
+if (string1.Length > string2.Length)
+{
+    string result = string1 + string2;
+    Console.WriteLine(result);
+}else if (string1.Length < string2.Length)
+{
+    string[] result = string1.Split(string2[0]);
+    for (int i = 0; i < result.Length; i++)
+    {
+        Console.Write(result[i]);
+    }
+
+}
+else
+{
+    Console.WriteLine("*some void*");
+}
